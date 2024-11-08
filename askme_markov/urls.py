f21a8d5b0cb47d07c2a359a5b1defa53ed76e6a9
Ilha <markovila539@gmail.com>
2024-11-08 17:57:04 +0300

    almost done, images no load except index

--- askme_markov/urls.py
+++ askme_markov/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('hot/', views.hot, name='hot'),
     path('question/<int:question_id>', views.question, name='question'),
+    path('tag/<str:tag_title>', views.tag, name='tag'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name='ask'),
     path('admin/', admin.site.urls),
 ]
